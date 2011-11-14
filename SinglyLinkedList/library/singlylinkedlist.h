69bf1e80166df0a86032ce9cd614a0faf9f55445
fititnt <emerson@webdesign.eng.br>
2011-11-14 21:06:13 -0200

    singlylinkedlist: improved documentation and added ziped generated docs

--- SinglyLinkedList/library/singlylinkedlist.h
+++ SinglyLinkedList/library/singlylinkedlist.h
@@ -1,8 +1,7 @@
 /**
- * @file singlylinkedlist.h
  * @details Singly Linked List
  * @see http://en.wikipedia.org/wiki/Linked_list
- * @version 0.1beta
+ * @version 0.1beta2
  * @author Emerson Rocha Luiz
  * @copyright MIT. See license.txt
  */
@@ -11,18 +10,38 @@
 #define SINGLYLINKEDLIST_H_INCLUDED
 
 //Struct of data do hold information
+/**
+ * Struct that hold data especification
+ * Change this struct for change witch tipe of data will be stored
+ *
+ * @param item[100] String data used on this class
+ */
 struct DataSLL
 {
     char item[100];
 } ;
+
+/**
+ * Define TypeSLLData as Type of DataSLL
+ */
 typedef struct DataSLL TypeSLLData; //Define one Type of this element
 
 //Struct of node used on list
+/**
+ * Struct that hold node especification
+ *
+ * @param data Data that this node hold
+ * @param next pointer for next node. NULL if this is last node
+ */
 struct NodeSLL
 {
     TypeSLLData data;
     struct NodeSLL* next;
 };
+
+/**
+ * Define TypeSLLNode as Type of NodeSLL
+ */
 typedef struct NodeSLL TypeSLLNode;  //Define one Type of this element
 
 //initializeSLL Node (set to NULL)
@@ -51,8 +70,8 @@ struct NodeSLL* initializeSLL(void)
 /**
  * Insert one new value to the list
  *
- * @param[in] TypeSLLNode list: List to search
- * @param[in] TypeSLLData data: Data for insert
+ * @param[in] list List to search
+ * @param[in] data Data for insert
  * @return TypeSLLNode list: Resultant list
  */
 struct NodeSLL* insertSLLNode (struct NodeSLL* list, TypeSLLData data)
@@ -86,7 +105,7 @@ struct NodeSLL* insertSLLNode (struct NodeSLL* list, TypeSLLData data)
 /**
  * Print all elements of one list
  *
- * @param[in] TypeSLLNode list: List to print
+ * @param[in] list List to print
  * @return void
  */
 void printSLLList(struct NodeSLL* list)
@@ -110,7 +129,7 @@ void printSLLList(struct NodeSLL* list)
 /**
  * Remove entire list, i.e., free memory usage of all elements
  *
- * @param[in] TypeSLLNode list: List to search
+ * @param[in] list List to search
  * @return void
  */
 void removeSLLList(struct NodeSLL* list)
@@ -128,9 +147,9 @@ void removeSLLList(struct NodeSLL* list)
 /**
  * Remove one node with one respective data value and return resultant list
  *
- * @param[in] TypeSLLNode list: List to search
- * @param[in] TypeSLLData data: Data for search to remove
- * @return TypeSLLNode list: Resultant list
+ * @param[in] list List to search
+ * @param[in] data Data for search to remove
+ * @return list Resultant list
  */
 struct NodeSLL* removeSLLNode(struct NodeSLL* list, TypeSLLData data)
 {
@@ -165,14 +184,16 @@ struct NodeSLL* removeSLLNode(struct NodeSLL* list, TypeSLLData data)
  * list, or respective node if found
  *
  * @example
- *      strcpy( data.item, "item to search");
- *      if ( searchSLLNode( list, data ) != NULL ){
- *          printf("\n%s found \n", data.item);
- *      }
+ * @code
+ * strcpy( data.item, "item to search");
+ * if ( searchSLLNode( list, data ) != NULL ){
+ *     printf("\n%s found \n", data.item);
+ * }
+ * @endcode
  *
- * @param[in] TypeSLLNode list: List to search
- * @param[in] TypeSLLData data: Data for search
- * @return TypeSLLNode aux: Node if find, NULL if not find element
+ * @param[in] list List to search
+ * @param[in] data Data for search
+ * @return aux Node if find, NULL if not find element
  */
 struct NodeSLL* searchSLLNode( struct NodeSLL* list, TypeSLLData data)
 {
